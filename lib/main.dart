daab2cbb405a158b5cab6e14a0c935fc5197896b
Tim <hshistudent@shacademy.org>
2023-01-23 14:20:37 -0800

    finished game reset

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(),
         home: MyHomePage(),
         routes: {
-          '/score':(context) => const Score(),
+          '/score': (context) => const Score(),
         },
       ),
     );
@@ -31,17 +31,22 @@ class MyAppState extends ChangeNotifier {
   final stopwatch = Stopwatch();
   var opacityList = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
   void toggleVisible(context, i) {
-if(!opacityList.contains(0.0)) {
-  stopwatch.start();
-}
-opacityList[i] = 0.0;
-if(!opacityList.contains(1.0)) {
-  stopwatch.stop();
-  print(stopwatch.elapsedMilliseconds / 1000.0);
-  Navigator.pushNamed(context, '/score');
-}
-notifyListeners();
+    if (!opacityList.contains(0.0)) {
+      stopwatch.start();
+    }
+    opacityList[i] = 0.0;
+    if (!opacityList.contains(1.0)) {
+      stopwatch.stop();
+      print(stopwatch.elapsedMilliseconds / 1000.0);
+      Navigator.pushNamed(context, '/score');
+    }
+    notifyListeners();
   }
+void newGame() {
+  stopwatch.reset();
+  opacityList = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,];
+  notifyListeners();
+}
 }
 
 class MyHomePage extends StatefulWidget {
@@ -78,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     onTap: () {
                       appState.toggleVisible(context, index);
                     },
-                 ),
+                  ),
                 ),
               ),
             ),
@@ -88,25 +93,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
 class Score extends StatelessWidget {
   const Score({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-  return Scaffold(
-    body: Column(
-      children: [
-        Text('Your time was; '
-        '${appState.stopwatch.elapsedMilliseconds / 1000.0}'
-        'seconds'),
-        ElevatedButton(
-          child: Text('New Game'),
-          onPressed:() {
-            Navigator.pop(context);
-          },
-        )
-      ],
-    ),
-  );
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Your time was; '
+              '${appState.stopwatch.elapsedMilliseconds / 1000.0}'
+              'seconds'),
+          ElevatedButton(
+            child: Text('New Game'),
+            onPressed: () {
+              appState.newGame();
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
   }
 }
